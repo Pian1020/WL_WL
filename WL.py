8c332ea083bb809ca958f88be1956f94a4b9b7a4
Pian1020 <147575714+Pian1020@users.noreply.github.com>
2024-05-06 01:15:32 +0800

    Update WL.py

--- WL.py
+++ WL.py
@@ -1,6 +1,12 @@
-import pandas as pd
 import zipfile
 import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.colors import LinearSegmentedColormap
+from pylab import *
+from datetime import datetime, timedelta
 
 def createDataset(path):
     dfs_wind = []
@@ -53,15 +59,37 @@ def createDataset(path):
                 with zip_ref.open(csv_filename) as f:
                     # Read CSV file, skip the first row and select specified columns
                     df = pd.read_csv(f, skiprows=1, usecols=columns_to_select)
-                    dfs_wind.append(df)
+
+                    # Process each row to create separate DataFrames
+                    for index, row in df.iterrows():
+                        heights = [10, 29, 38, 49, 79, 99, 119, 139, 159, 179, 199]
+                        wind_directions = [row[col] for col in df.columns if 'Wind Direction' in col]
+                        horizontal_speeds = [row[col] for col in df.columns if 'Horizontal Wind Speed' in col]
+                        vertical_speeds = [row[col] for col in df.columns if 'Vertical Wind Speed' in col]
+                        datetime_str = row["Time and Date"]
+                        datetime_obj = [datetime.strptime(datetime_str, "%d/%m/%Y %H:%M:%S")] * len(heights)
+                        
+                        # Create a DataFrame for each row
+                        height_df = pd.DataFrame({
+                            'Height': heights,
+                            'Wind Direction': wind_directions,
+                            'Horizontal Wind Speed': horizontal_speeds,
+                            'Vertical Wind Speed': vertical_speeds,
+                            'datetime': datetime_obj
+                        })
+
+                        # Append the DataFrame to the list
+                        dfs_wind.append(height_df)
+    # Convert the list of DataFrames to a single DataFrame
+    df_multi = pd.concat(dfs_wind)
     
-    return dfs_wind
+    return df_multi
 
 if __name__ == "__main__":
     # Determine the file path
     root = os.getcwd()
     file_path = root + "/ProcessedData"
 
-    # Read files from the disk
-    dfs_wind = createDataset(file_path)
-    print(dfs_wind)
+    # Read files from the disk and process the data
+    dataset = createDataset(file_path)
+    print(dataset)
